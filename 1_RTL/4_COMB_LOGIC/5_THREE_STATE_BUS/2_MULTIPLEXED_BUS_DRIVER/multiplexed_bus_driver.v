826581bbadb14901494e25a42a9b7ade718987b5
William Woo <codefoolosopher@gmail.com>
2026-07-01 13:58:51 +0900

    Delete unnecessary files

--- 1_RTL/4_COMB_LOGIC/5_THREE_STATE_BUS/2_MULTIPLEXED_BUS_DRIVER/multiplexed_bus_driver.v
+++ 1_RTL/4_COMB_LOGIC/5_THREE_STATE_BUS/2_MULTIPLEXED_BUS_DRIVER/multiplexed_bus_driver.v
@@ -22,11 +22,11 @@ wire [7:0]  w_mux_out       ;
 assign w_select = i_en_a ^ i_en_b                   ;
 
     
-assign o_mux_out =  (i_en_a) ? i_data_a :
+assign w_mux_out =  (i_en_a) ? i_data_a :
                     (i_en_b) ? i_data_b :
                     8'bz                            ; 
 
     
-assign o_bus_data = (w_select) ? o_mux_out : 8'bz   ;
+assign o_bus_data = (w_select) ? w_mux_out : 8'bz   ;
 
 endmodule
